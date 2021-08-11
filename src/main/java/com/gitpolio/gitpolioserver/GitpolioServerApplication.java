8e29440c097f604fe61364dff629b48bf641de90
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-08-11 13:51:57 +0900

    기본 프로젝트 환경 구성 - Gradle 추가 - Spring Initializer 를 통한Spring Boot 추가

---
+++ src/main/java/com/gitpolio/gitpolioserver/GitpolioServerApplication.java
@@ -0,0 +1,13 @@
+package com.gitpolio.gitpolioserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpolioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpolioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitpolio/gitpolioserver/GitpolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpolio.gitpolioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpolioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
